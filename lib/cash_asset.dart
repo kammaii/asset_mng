56a86c46793dd9a8ebed5426f7a216670a8179c5
kammaii <kammaii@naver.com>
2021-08-28 21:58:56 +0700

    made table

---
+++ lib/cash_asset.dart
@@ -0,0 +1,28 @@
+
+class CashAsset {
+
+  late String currency;
+  late double amount;
+  late double exchangeRate;
+  late String assetType;
+
+  CashAsset(this.currency, this.amount, this.exchangeRate, this.assetType);
+
+  static const String CURRENCY = 'currency';
+  static const String AMOUNT = 'amount';
+  static const String EXCHANGE_RATE = 'exchangeRate';
+  static const String ASSET_TYPE = 'assetType';
+
+  CashAsset.fromJson(Map<String, dynamic> json) :
+    currency = json[CURRENCY],
+    amount = json[AMOUNT],
+    exchangeRate = json[EXCHANGE_RATE],
+    assetType = json[ASSET_TYPE];
+
+  Map<String, dynamic> toJson() => {
+    CURRENCY : currency,
+    AMOUNT : amount,
+    EXCHANGE_RATE : exchangeRate,
+    ASSET_TYPE : assetType
+  };
+}
--- lib/main_cash_flow.dart
+++ lib/main_cash_flow.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:asset_mng/cash_asset.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -13,18 +15,48 @@ class _CashFlowState extends State<CashFlow> {
   List<String> dropdownItems = ['1', '2', '3', '4'];
   double assetGoal = 100000000;
   var f = NumberFormat('###,###,###,###.##');
+  final textFieldController = TextEditingController();
+  late String monthlyGoal;
+
+  List<Map<String, dynamic>> cashDataList = [];
+
+  static const String CASH_REPORT = 'cashReport';
+
+  List<String> cashJsonList = [ // todo: 샘플 현금 현황 (실재 DB에서 받는 데이터 형태)
+    jsonEncode(CashAsset('원화', 1000000, 1, '생활비')),
+    jsonEncode(CashAsset('바트', 5555, 37, '생활비')),
+    jsonEncode(CashAsset('달러', 3000, 1065, '투자자산'))
+  ];
+
+  List<double> exchangeRate = [1, 38.5, 1070.4]; // todo: 현재 환율 웹스크래핑으로 가져오기
+
+
+  @override
+  void initState() {
+    super.initState();
+    monthlyGoal = ""; // todo: 저장된 값 가져오기
+    textFieldController.addListener(() {
+      monthlyGoal = textFieldController.text;
+    });
+
+    // json 리스트를 Map 리스트로 변경
+    for(String cashData in cashJsonList) {
+      cashDataList.add(jsonDecode(cashData));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
-
+                  // todo: 새 데이터 입력하기
                 },
                 child: Text('입력하기')),
             ],
@@ -56,11 +88,72 @@ class _CashFlowState extends State<CashFlow> {
           SizedBox(height: 20.0),
           Row(
             children: [
-              Text('목표: ${f.format(assetGoal)}')
+              Text('목표: ${f.format(assetGoal)} 원 (월'),
+              SizedBox(width: 10),
+              Container(
+                width: 70,
+                height: 30,
+                alignment: Alignment.center,
+                child: TextField(
+                  decoration: new InputDecoration(
+                    labelText: monthlyGoal,
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.only(
+                      bottom: 15,  // HERE THE IMPORTANT PART
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(width: 5),
+              Text('만원)'),
+              SizedBox(width: 20),
+              ElevatedButton(
+                child: Text('저장'),
+                onPressed: () {
+                  // todo: monthlyGoal 저장하기
+                },
+              ),
             ],
-          )
+          ),
+          SizedBox(height: 50),
+          Text('현금현황', textScaleFactor: 2),
+          makeTable(CASH_REPORT)
         ],
       ),
+    );
+  }
+
+  DataTable makeTable(String type) {
+    List<DataColumn> dataColumn = [];
+    List<DataRow> dataRow = [];
+
+    switch (type) {
+      case CASH_REPORT :
+        List<String> columns = ['','총액','증감','환율'];
+        dataColumn = List<DataColumn>.generate(columns.length, (index) => getColumn(columns[index]));
+        dataRow = List<DataRow>.generate(cashDataList.length, (index) =>
+          DataRow(
+            cells: [
+              DataCell(Text(cashDataList[index][CashAsset.CURRENCY])),
+              DataCell(Text(cashDataList[index][CashAsset.AMOUNT].toString())),
+              DataCell(Text(cashDataList[index][CashAsset.AMOUNT].toString())), // todo: 환율 및 현금변동내역 반영할 것
+              DataCell(Text(exchangeRate[index].toString())),
+            ]
+          )
+        );
+        break;
+    }
+
+    return DataTable(
+      columns: dataColumn,
+      rows: dataRow,
+    );
+  }
+
+  DataColumn getColumn(String label) {
+    return DataColumn(
+      label: Text(label, style: TextStyle(fontWeight: FontWeight.bold))
     );
   }
 }
