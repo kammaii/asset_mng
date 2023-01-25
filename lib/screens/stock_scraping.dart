bdeb658259dd564980cfa66e631ba0a77e19e2a1
kammaii <kammaii@naver.com>
2023-01-25 22:41:46 +0900

    refactoring...

--- lib/screens/stock_scraping.dart
+++ lib/screens/stock_scraping.dart
@@ -1,3 +1,4 @@
+import 'package:asset_mng/scraping_api.dart';
 import 'package:flutter/material.dart';
 
 class StockScraping extends StatelessWidget {
@@ -5,6 +6,28 @@ class StockScraping extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        ElevatedButton(
+          onPressed: () async {
+            List<dynamic> stockCodes = await ScrapingApi().getStockCodes();
+            for (int i = 0; i < stockCodes.length; i = i + 10) {
+              await runScraping(stockCodes, i, i + 10);
+            }
+          },
+          child: Text('스크래핑 실행'),
+        ),
+      ],
+    );
+  }
+
+  Future<bool> runScraping(List<dynamic> stockCodes, int startIndex, int endIndex) async {
+    List<Future<bool>> results = [];
+    for(int i=startIndex; i<endIndex && i<stockCodes.length; i++) {
+      Future<bool> result = ScrapingApi().getScraping(stockCodes[i]);
+      results.add(result);
+    }
+    await Future.wait(results);
+    return Future.value(true);
   }
 }
